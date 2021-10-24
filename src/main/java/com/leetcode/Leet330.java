70df3f70f3ea08fa9182df21e0b5b215e6f1b066
indranil.das <indranil.das@ext.deposit.solutions.com>
2021-10-24 14:49:40 +0530

    commit

--- src/main/java/com/leetcode/Leet330.java
+++ src/main/java/com/leetcode/Leet330.java
@@ -1,19 +1,71 @@
 package com.leetcode;
 
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Leet330 {
 
     public static void main(String[] args) {
-        int[] nums = {1,2};
+        int[] nums = {1,5,7};
 
-        int n = 8;
+        int n = 18;
         int k = 0;
         //minPatches(nums,n);
         k = minPatches_DP(nums, n);
-        System.out.println(k);
+
+        sol(nums,n);
     }
 
+
+    static void sol(int[] nums, int n) {
+        int res =0;
+        int len = nums.length;
+      //  List<Integer> list = new LinkedList<>(Arrays.stream(nums).boxed().collect(Collectors.toList()));
+        int sum=nums[0];
+        int j=0;
+        int r =0;
+        boolean endOfArray = false;
+        while(r<=n || j<nums.length){
+            if(r>=n) break;
+            if(endOfArray){
+                if(j== nums.length-1){
+                    r=r+nums[j];
+                    j++;
+                    continue;
+                }
+                r=2*r+1;
+                res++;
+                continue;
+            }
+            if(r==sum ){
+                j=j+1;
+                sum=sum+nums[j];
+                if(j==nums.length-1) endOfArray=true;
+               // r++;
+            }
+            if(r<sum){
+                if(2*r+1<r+nums[j]){
+                    r=2*r+1;
+                    res++;
+                }else {
+                    r=r+nums[j];
+                    j++;
+                    if(j==nums.length-1) endOfArray=true;
+                }
+
+            }
+            if(r>sum){
+              r=r+nums[j];
+              j++;
+              sum=sum+nums[j];
+              if(j==nums.length-1) endOfArray=true;
+            }
+        }
+        System.out.println("joy---" +res);
+
+    }
 
     public static int minPatches_DP(int[] nums, int n) {
 
@@ -24,7 +76,8 @@ public class Leet330 {
 
         while (!allTrue(dp)) {
 
-
+            dp = new boolean[n + 1];
+            dp[0] = true;
 
             for (int i = 0; i < nums.length; i++) {
 
@@ -32,8 +85,14 @@ public class Leet330 {
                 for (j = 1; j <= n; j++) {
                     if (nums[i] <= j)
                         temp[j] = dp[j - nums[i]];
+
                 }
                 dp = temp;
+                System.out.println("i=" + i + ",j=" + j);
+                System.out.println(Arrays.toString(dp));
+                System.out.println(Arrays.toString(nums));
+                System.out.println("----------");
+
             }
             ///find first false
             int c = 1;
@@ -41,17 +100,17 @@ public class Leet330 {
                 if (!dp[c]) break;
             }
             if (allTrue(dp)) break;
-            nums=addCToNums(nums, c);
-            System.out.println("----------");
-            System.out.println(Arrays.toString(nums));
-            System.out.println(Arrays.toString(dp));
-            System.out.println("----------");
+            nums = addCToNums(nums, c);
+//            System.out.println("----------");
+//            System.out.println(Arrays.toString(nums));
+//            System.out.println(Arrays.toString(dp));
+//            System.out.println("----------");
         }
-        System.out.println("----------");
-        System.out.println(Arrays.toString(dp));
-        System.out.println(Arrays.toString(nums));
-        System.out.println("----------");
-        return nums.length-initLen;
+//        System.out.println("----------");
+//        System.out.println(Arrays.toString(dp));
+//        System.out.println(Arrays.toString(nums));
+//        System.out.println("----------");
+        return nums.length - initLen;
 
     }
 
@@ -80,50 +139,49 @@ public class Leet330 {
 ///////////////////////////////////////////////////////////////////////
 
 
-
-    public static int minPatches(int[] nums, int n) {
-
-        int initLen = nums.length;
-        boolean[] dp = new boolean[n + 1];
-        dp[0] = true;
-        int j = 1;
-
-        for (int i = 0; i < nums.length; i++) {
-            boolean[] temp = new boolean[n + 1];
-            int t = 0;
-            ///copy dp to temp -->o(n)
-            for (int c = 0; c <= j; c++) {
-                temp[c] = true;
-            }
-            ///c---> o(n)
-            dp = temp;
-            boolean addN = false;
-            while (j <= n) {
-                if (nums[i] >= j) temp[j] = dp[j - nums[i]];
-                if (temp[j] && !temp[j - 1]) {
-                    addN = true;
-                    break;
-                }
-                j++;
-            }
-
-            // nums =createNewAndInsert(nums, j-1);
-            if (addN) {
-                int[] nn = new int[nums.length + 1];
-                System.arraycopy(nn, 0, nums, 0, i - 1);
-                nn[i] = j - 1;
-                System.arraycopy(nn, i + 1, nums, i, nums.length - 1 - i);
-                nums = nn;
-                i--;
-                j--;
-            } else {
-                j = 1;
-            }
-        }
-
-        return nums.length - initLen;
-
-    }
+//    public static int minPatches(int[] nums, int n) {
+//
+//        int initLen = nums.length;
+//        boolean[] dp = new boolean[n + 1];
+//        dp[0] = true;
+//        int j = 1;
+//
+//        for (int i = 0; i < nums.length; i++) {
+//            boolean[] temp = new boolean[n + 1];
+//            int t = 0;
+//            ///copy dp to temp -->o(n)
+//            for (int c = 0; c <= j; c++) {
+//                temp[c] = true;
+//            }
+//            ///c---> o(n)
+//            dp = temp;
+//            boolean addN = false;
+//            while (j <= n) {
+//                if (nums[i] >= j) temp[j] = dp[j - nums[i]];
+//                if (temp[j] && !temp[j - 1]) {
+//                    addN = true;
+//                    break;
+//                }
+//                j++;
+//            }
+//
+//            // nums =createNewAndInsert(nums, j-1);
+//            if (addN) {
+//                int[] nn = new int[nums.length + 1];
+//                System.arraycopy(nn, 0, nums, 0, i - 1);
+//                nn[i] = j - 1;
+//                System.arraycopy(nn, i + 1, nums, i, nums.length - 1 - i);
+//                nums = nn;
+//                i--;
+//                j--;
+//            } else {
+//                j = 1;
+//            }
+//        }
+//
+//        return nums.length - initLen;
+//
+//    }
 
 //    private int[] createNewAndInsert(int[] nums, int n) {
 //
