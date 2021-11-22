e523e2849b9e531dc9e68aa2973ccc2b10999dc5
Indranil <java.indranil@gmail.com>
2021-11-22 23:46:17 +0530

    commit

--- src/main/java/com/java/CustomBlockingQueue_Conditional.java
+++ src/main/java/com/java/CustomBlockingQueue_Conditional.java
@@ -4,6 +4,27 @@ import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
+//
+//Condition factors out the Object monitor methods (wait, notify and notifyAll) into distinct objects to give
+// the effect of having multiple wait-sets per object, by combining them with the use of arbitrary Lock implementations.
+// Where a Lock replaces the use of synchronized methods and statements, a Condition replaces the use of the Object monitor methods.
+//
+//Conditions (also known as condition queues or condition variables) provide a means for one thread to suspend execution (to "wait")
+// until notified by another thread that some state condition may now be true. Because access to this shared state information
+// occurs in different threads, it must be protected, so a lock of some form is associated with the condition.
+// The key property that waiting for a condition provides is that it atomically releases the associated lock and suspends
+// the current thread, just like Object.wait.
+//
+//A Condition instance is intrinsically bound to a lock. To obtain a Condition instance for a particular Lock instance use
+// its newCondition() method.
+//
+//As an example, suppose we have a bounded buffer which supports put and take methods.
+// If a take is attempted on an empty buffer, then the thread will block until an item becomes available;
+// if a put is attempted on a full buffer, then the thread will block until a space becomes available.
+// We would like to keep waiting put threads and take threads in separate wait-sets so that
+// we can use the optimization of only notifying a single thread at a time when items or spaces become available in the buffer.
+// This can be achieved using two Condition instances.
+
 class CustomBlockingQueue {
 
     final Lock lock = new ReentrantLock();
