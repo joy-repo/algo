e659449e31ef015a1c029db16ca85fd818e1f64a
Indranil <java.indranil@gmail.com>
2023-11-21 20:27:14 +0530

    bs

--- src/main/java/blink75/binarysearch/BinarySearch.java
+++ src/main/java/blink75/binarysearch/BinarySearch.java
@@ -4,10 +4,29 @@ public class BinarySearch {
 
   public static int[] ARR = {3,4,6,7,9,12,16};
 
-  public static int N = 5;
+  public static int N = 12;
 
   public static void main(String[] args) {
     sol();
+    int val = solRecc(0, ARR.length-1);
+    System.out.println("RECC ans :"+ val);
+  }
+
+  private static int solRecc(int low, int high) {
+
+    if(low>high){
+      return -1;
+    }
+    int mid = (low+high)/2;
+    if(ARR[mid]==N){
+      return mid;
+    }
+    if(N > ARR[mid]){
+      return solRecc(mid+1, high);
+    }else {
+      return solRecc(low, mid-1);
+    }
+
   }
 
   private static void sol() {
