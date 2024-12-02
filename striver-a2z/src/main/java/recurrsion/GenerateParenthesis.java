e0762397621ee4c2792dd468991bad8f90bfc2e3
Titlee_Joy <daasindrranil@gmail.com>
2024-12-03 00:07:33 +0530

    cc

---
+++ striver-a2z/src/main/java/recurrsion/GenerateParenthesis.java
@@ -0,0 +1,28 @@
+package recurrsion;
+
+public class GenerateParenthesis {
+
+    static int N = 3;
+
+    public static void main(String[] args) {
+        recSol("",0, 0);
+    }
+
+    private static void recSol(String str, int opening, int closing) {
+        if(closing>opening) return;
+        if(closing>=N){
+            System.out.println(str);
+        }
+
+        if(opening ==N){
+            recSol(str+")", opening, closing+1);
+        } else if(closing==opening){
+            recSol(str+"(", opening+1, closing);
+        } else {
+            recSol(str + "(", opening + 1, closing);
+            recSol(str + ")", opening, closing + 1);
+        }
+    }
+
+}
+
