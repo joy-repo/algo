a6653202346e3f0222f444f5e1116f5be9ad3331
Titlee_Joy <daasindrranil@gmail.com>
2024-12-09 23:27:47 +0530

    cc

--- striver-a2z/src/main/java/recurrsion/WordSearch.java
+++ striver-a2z/src/main/java/recurrsion/WordSearch.java
@@ -1,5 +1,6 @@
 package recurrsion;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -17,20 +18,33 @@ public class WordSearch {
 
     public static void main(String[] args) {
         Set<Pair> set = new HashSet<>();
-        doWordSearch(set, 0, 0, 0)
+        Boolean res = doWordSearch(set, 0, 0, 0);
+        System.out.println(res);
     }
 
     private static boolean doWordSearch(Set<Pair> set, int x, int y, int targetIndex) {
-       if(targetIndex==target.length()) return true;
+       if(targetIndex==target.length()) {
+           System.out.println(set);return true;
+       }
       // if(set.size()>= board[0].length*board.length) return false;
+       if(set.contains(new Pair(x,y))) return false;
 
-       if()
+       if(x<0||x>=board[0].length||y<0||y>=board.length) return false;
+        if(target.charAt(targetIndex)!= board[y][x]) return false;
+       if(target.charAt(targetIndex)== board[y][x]) {
+           set.add(new Pair(x, y));
+       }
+
+           return doWordSearch(set, x + 1, y, targetIndex + 1)
+                   || doWordSearch(set, x - 1, y, targetIndex + 1)
+                   || doWordSearch(set, x, y + 1, targetIndex + 1)
+                   || doWordSearch(set, x, y - 1, targetIndex + 1);
 
     }
 
-
+    @AllArgsConstructor
     @EqualsAndHashCode
-    class Pair{
+    static class Pair{
         int x;
         int y;
     }
