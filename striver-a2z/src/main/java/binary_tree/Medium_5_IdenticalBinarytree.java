4ab807c3bf77f48e5ccd1d4dc2ead1927d8eb5be
Indranil Das <dindrada@example.com>
2024-12-31 11:54:14 +0530

    commit

---
+++ striver-a2z/src/main/java/binary_tree/Medium_5_IdenticalBinarytree.java
@@ -0,0 +1,11 @@
+package binary_tree;
+
+import binary_tree.MyBinaryTree.Node;
+
+public class Medium_5_IdenticalBinarytree {
+
+    public static void main(String[] args) {
+        Node node = MyBinaryTree.generateBinaryTree();
+
+    }
+}
---
+++ striver-a2z/src/main/java/binary_tree/Medium_6_ZigzagTraversal.java
@@ -0,0 +1,16 @@
+package binary_tree;
+
+import binary_tree.MyBinaryTree.Node;
+
+public class Medium_6_ZigzagTraversal {
+
+    public static void main(String[] args) {
+        Node root = MyBinaryTree.generateBinaryTree();
+        zigzagTraversal(root);
+    }
+
+    private static void zigzagTraversal(Node root) {
+
+
+    }
+}
