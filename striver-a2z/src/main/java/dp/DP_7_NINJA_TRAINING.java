25e7a0f93cf25cb1471bf218747f33d1a7910fd0
Indranil Das <dindrada@example.com>
2025-01-17 20:30:36 +0530

    commit

--- striver-a2z/src/main/java/dp/DP_7_NINJA_TRAINING.java
+++ striver-a2z/src/main/java/dp/DP_7_NINJA_TRAINING.java
@@ -7,9 +7,15 @@ public class DP_7_NINJA_TRAINING {
 
     static int[][] POINTS = {
             {5,2,1}, //
-            {3,3,1}, //
-            {1,3,3}  //
+            {6,1,3},
+            {2,1,3},
+
     };
+
+    /// 0    0    0  0
+    /// 0    0    0  0
+    /// 0    0    0  0
+    /// 0    0    0  0
 
     public static void main(String[] args) {
         int res = byRecc(0, -1);
@@ -18,7 +24,7 @@ public class DP_7_NINJA_TRAINING {
         for(int[] t : memo){
             Arrays.fill(t, -1);
         }
-        res = memorization(0,0,memo);
+        res = memorization(0,-1,memo);
         System.out.println("/////MEMO/////");
         System.out.println(res);
         for(int[] t : memo){
@@ -46,7 +52,7 @@ public class DP_7_NINJA_TRAINING {
     private static int memorization(int dayIdx, int lastIdx, int[][] memo) {
         if(dayIdx==POINTS.length) return 0;
         int res = 0;
-        if(memo[dayIdx][lastIdx]!=-1) return memo[dayIdx][lastIdx];
+        if( lastIdx!=-1 &&memo[dayIdx][lastIdx]!=-1 ) return memo[dayIdx][lastIdx];
 
         for(int i =0; i<POINTS[dayIdx].length; i++){
             if(i!=lastIdx || lastIdx==-1) {
@@ -54,8 +60,37 @@ public class DP_7_NINJA_TRAINING {
                 res = Math.max(tmp, res);
             }
         }
-        memo[dayIdx][lastIdx] = res;
-        return memo[dayIdx][lastIdx];
+        if(lastIdx!=-1)
+            memo[dayIdx][lastIdx] = res;
+        return res;
+    }
+
+    public static void bydp(){
+
+        int[][] dp = new int[POINTS.length+1][POINTS[0].length+1];
+
+        for(int y=1; y<dp.length; y++){
+            for (int x=1; x<dp[0].length; x++){
+                int res = 0;
+                for(int i =0; i<POINTS[y-1].length; i++){
+                    if(i!=y-1 || y==1) {
+                        int tmp = dp[y - 1][x] + POINTS[y-1][i];
+                        res = Math.max(tmp, res);
+                    }
+                }
+                dp[y][x]=res;
+            }
+        }
+                System.out.println();
+        System.out.print("/////DP/////");
+
+        for(int[] t : dp){
+            System.out.println();
+            for(int i : t) {
+                System.out.print(i + ",");
+            }
+        }
+
     }
 
     private static void byDP() {
@@ -68,7 +103,7 @@ public class DP_7_NINJA_TRAINING {
 
                     if(i!=y-1 || y==1) {
                         //int tmp = byRecc(dayIdx + 1, i) + POINTS[dayIdx][i];
-                        int tmp = DP[y - 1][i+1] + POINTS[y-1][i];
+                        int tmp = DP[y - 1][x] + POINTS[y-1][i];
                         res = Math.max(tmp, res);
                     }
                 }
---
+++ striver-a2z/src/main/java/dp/random_probs/LongestPanindromicSubsequence.java
@@ -0,0 +1,28 @@
+package dp.random_probs;
+
+
+public class LongestPanindromicSubsequence {
+
+    static char[] STR = "BBABCBCAB".toCharArray();
+
+    public static void main(String[] args) {
+        int res = lpsByRecc(0,STR.length-1, "");
+        System.out.println(res);
+    }
+
+    private static int lpsByRecc(int start, int end, String res) {
+        if(start>end) return 0;
+        if(start==end) { System.out.println(res);return 1;}
+
+        if(STR[start]==STR[end])
+            return lpsByRecc(start+1,end-1, res+STR[start]+STR[end])+2;
+
+        int tmp1 = lpsByRecc(start+1, end, res);
+        int tmp2 = lpsByRecc(start, end-1, res);
+        return Math.max(tmp1,tmp2);
+    }
+
+    private static void byDP(){
+
+    }
+}
