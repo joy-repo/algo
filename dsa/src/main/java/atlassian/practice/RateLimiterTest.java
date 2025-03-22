f9588d9ad1fb6702aed75c3fd7508ea73f7b7172
Indranil Das <dindrada@example.com>
2025-03-23 00:14:56 +0530

    commit

--- dsa/src/main/java/atlassian/practice/RateLimiterTest.java
+++ dsa/src/main/java/atlassian/practice/RateLimiterTest.java
@@ -2,6 +2,9 @@ package atlassian.practice;
 
 import org.junit.Test;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class RateLimiterTest {
 
 
@@ -9,8 +12,18 @@ public class RateLimiterTest {
     @Test
     public void testRateLimiting(){
 
+        TokenBucketPractice limiter = new TokenBucketPractice(3, 1);
+        String userId1 = "User123";
+        String userId2 = "User456";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
 
-
+        for (int i = 0; i < 25; i++) {
+            System.out.println("Time: "+ LocalDateTime.now().format(formatter)+ " : User 1 - Request " + (i + 1) + ": " + (limiter.rateLimitUser(userId1) ? "Allowed" : "Rejected"));
+            //  System.out.println("User 2 - Request " + (i + 1) + ": " + (limiter.allowRequest(userId2) ? "Allowed" : "Rejected"));
+            try {
+                Thread.sleep(100); // Simulate request interval
+            } catch (InterruptedException ignored) {}
+        }
     }
 
 }
--- dsa/src/main/java/atlassian/practice/TokenBucketPractice.java
+++ dsa/src/main/java/atlassian/practice/TokenBucketPractice.java
@@ -1,45 +1,47 @@
 package atlassian.practice;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 
-@Data
-@AllArgsConstructor
+
+
 public class TokenBucketPractice {
 
     private int maxBucketSize ;
-    private int windowSizeInMilli;
+    private int windowSizeInSeconds;
     ScheduledExecutorService scheduler;
 
 
     private ConcurrentHashMap<String, Integer> userTokensMap ;
 
-    public TokenBucketPractice(int maxBucketSize, int windowSizeInMilli){
+    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
+
+    public TokenBucketPractice(int maxBucketSize, int windowSizeInSeconds){
         this.maxBucketSize=maxBucketSize;
-        this.windowSizeInMilli=windowSizeInMilli;
+        this.windowSizeInSeconds =windowSizeInSeconds;
         this.userTokensMap = new ConcurrentHashMap<>();
         scheduler = Executors.newScheduledThreadPool(1);
 
-        scheduler.scheduleAtFixedRate(this::refillTokens, 0,windowSizeInMilli, TimeUnit.MILLISECONDS );
+        scheduler.scheduleAtFixedRate(this::refillTokens, 0,this.windowSizeInSeconds, TimeUnit.SECONDS );
 
     }
 
     public  boolean rateLimitUser(String userId){
         userTokensMap.putIfAbsent(userId, maxBucketSize);
-        return userTokensMap.compute(userId, (u, t)-> (t>0) ? t-1 : t) >0;
+        return userTokensMap.compute(userId, (u, t)->  t-1) >=0;
 
     }
 
     private  void refillTokens() {
        // userTokensMap.merge(userId, maxBucketSize, (oldTokens, max) -> Math.max(oldTokens, max))
 
+        //System.out.println("refillTokens - Time: "+ LocalDateTime.now().format(formatter));
         for(String userId : userTokensMap.keySet()){
             userTokensMap.merge(userId, maxBucketSize, (oldTokens, max) -> maxBucketSize);
             //userTokensMap.put(userId, maxBucketSize);
