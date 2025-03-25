5c769a1ec29a77bfc1cec7a82d3341aec3a67d2c
Indranil Das <dindrada@example.com>
2025-03-25 13:46:15 +0530

    commit

---
+++ dsa/src/main/java/test/leetCode179.java
@@ -0,0 +1,25 @@
+package test;
+
+import java.util.PriorityQueue;
+
+public class leetCode179 {
+
+    static int[] NUM = {3,30,34,5,9};
+
+    public static void main(String[] args) {
+        solve179();
+    }
+
+    private static void solve179() {
+
+//        PriorityQueue<Integer> pq = new PriorityQueue<>((c1,c2) ->{
+//
+//        });
+
+
+    }
+
+    private static int getLeftMostNum(int n ){
+        return n/10;
+    }
+}
